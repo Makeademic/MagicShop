ac68252efff25dbf6259b050715e9c6c74e80e87
Makeademic <kaplanwizard@gmail.com>
2024-02-07 11:30:37 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-function greatestAchievement() {
-  var password = document.getElementById("passwordBox2");
-  /*add .toUpperCase() String function so that even if the user types in the password in lower-case, it will check against uppercase password text*/
-  var passwordText = password.value.toUpperCase();
-  if (passwordText == "50") {
-    return true;
-  }
-  alert("That is incorrect, try again!");
-  return false;
+function bgChange(){ document.body.style.backgroundColor = "AntiqueWhite";
+}
+
+function bgChange(){ document.getElementById("btn").style.backgroundColor = "AntiqueWhite";
 }
